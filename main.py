d5515559e3ae6f1633510e8f3836348e3d2a72b5
Gabriela Ramos <gabrielaramos@Gabrielas-MacBook-Air.local>
2022-06-23 17:01:57 -0500

    token rules main.py

--- main.py
+++ main.py
@@ -173,6 +173,126 @@ tokens = (
     'NULL',  # \0 end of line
 ) + tuple(reserved.values())
 
+# Regular expression rules for simple tokens
+
+# Arithmetic Operators
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'\/'
+t_MODULUS = r'%'
+
+# Assignment Operators
+t_EQUAL = r'='
+t_PLUS_EQUAL = r'\+='
+t_MINUS_EQUAL = r'-='
+t_TIMES_EQUAL = r'\*='
+t_DIV_EQUAL = r'\/='
+t_MOD_EQUAL = r'\%='
+
+# Bitwise Assignment Operators
+t_AND_EQUAL = r'\&='
+t_OR_EQUAL = r'\|='
+t_XOR_EQUAL = r'\^='
+t_COMPLEMENT_EQUAL = r'\~='
+t_SHIFTL_EQUAL = r'\<\<\='
+t_SHIFTR_EQUAL = r'\>\>\='
+
+# Comparison Operators
+t_EQUAL_TO = r'=='
+t_NOT_EQUAL = r'!='
+t_GREATER_THAN = r'>'
+t_LESS_THAN = r'<'
+t_GREATER_EQUAL = r'>='
+t_LESS_EQUAL = r'<='
+
+# Logical Operators
+t_AND = r'&&'
+t_OR = r'\|\|'
+t_NOT = r'\!'
+
+# Bitwise Operators
+t_B_AND = r'\&'
+t_B_OR = r'\|'
+t_B_XOR = r'\^'
+t_B_COMPLEMENT = r'\~'
+t_SHIFT_LEFT = r'\<\<'
+t_SHIFT_RIGHT = r'\>\>'
+
+# Groups
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
+t_LCURL_BRACE = r'\{'
+t_RCURL_BRACE = r'\}'
+
+# Values
+t_VARNAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_INTEGER = r'[0-9]+'
+t_DECIMAL = r'[0-9]*\.[0-9]+'
+t_CHARACTER = r'\'\w+\''
+t_STRING = r'\".*\"'
+
+# Miscellaneous
+t_SINGLE_QUOTE = r'\''
+t_DOUBLE_QUOTE = r'\"'
+t_COLON = r'\:'
+t_SEMICOLON = r'\;'
+t_DOT = r'\.'
+t_COMMA = r'\,'
+t_QUESTIONMARK = r'\?'
+t_AMPERSAND = r'\&'
+t_TILDE = r'\~'
+#t_COMMENT = r'\/\/.*|\/\*(\*(?!\/)|[^*])*\*\/'
+t_ignore = " \t"
+
+# Preprocessor Directives
+t_PP_INCLUDE = r'\#include'
+t_PP_DEFINE = r'\#define'
+t_PP_UNDEF = r'\#undef'
+t_PP_IF = r'\#if'
+t_PP_IFDEF = r'\#ifdef'
+t_PP_IFNDEF = r'\#ifndef'
+t_PP_ERROR = r'\#error'
+t_PP_PRAGMA = r'\#pragma'
+# Preprocessor Macros
+t_PP_FILE = r'__FILE__'
+t_PP_LINE = r'__LINE__'
+t_PP_DATE = r'__DATE__'
+t_PP_TIME = r'__TIME__'
+t_PP_TIMESTAMP = r'__TIMESTAMP__'
+t_PP_STMACRO = r'\S+\s\S+' # Single token Macro
+t_PP_DTMACRO = r'\#\S+\s.+'  # Double token Macro
+
+t_HEADER_LIB = r'<[a-z\_\/]+\.h>|\"[a-z\_\/]+\.h\"|\'[a-z\_\/]+\.h\''
+
+# Format specifiers
+t_FS_CHAR          = r'\%c'
+t_FS_INT           = r'\%d|\%i'
+t_FS_LONG          = r'\%ld|\%D'
+t_FS_FLOAT         = r'\%f'
+t_FS_SCI_NOTATION  = r'\%e|\%E|\%g|\%G'
+t_FS_STRING        = r'\%s'
+t_FS_UNSIGNED_INT  = r'\%u'
+t_FS_UNSIGNED_LONG = r'\%lu|\%U'
+t_FS_OCT           = r'\%o' 
+t_FS_HEX           = r'\%x|\%X'
+t_FS_POINTER       = r'\%p'
+t_FS_OCT_LONG      = r'\%lo|\%O'
+t_FS_DOUBLE        = r'\%lf'
+t_FS_LONG_DOUBLE   = r'\%LF'
+
+# Secuencias de escape
+t_NEWLINE = r'\\n'
+t_BACKSPACE = r'\\b'
+t_HTAB = r'\\t'
+t_VTAB = r'\\v' 
+t_BACKSLASH  = r'\\\\'
+t_FF_PAGEBREAK = r'\\f'  # Formfeed Page Break
+t_SINGLE_APOS = r'\\\''
+t_DOUBLE_APOS = r'\\\"'
+t_NULL = r'\\0' # end of line or null
 
 def t_lineCounter(t):
     r'\n+'
